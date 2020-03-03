164a5c7b265571ac2495b9b1f57aa69e2601e614
Luigi Quattrociocchi <lquatt2@gmail.com>
2020-03-03 10:16:32 -0500

    fix gridsum

--- GridSums.java
+++ GridSums.java
@@ -8,13 +8,13 @@ public class GridSums {
 		for (int r = 0; r < z; r++)
 			for (int c = 0; c < z; c++)
 				grid[r][c] = in.nextInt();
-		
+
 		int t = in.nextInt();
-		int[][] pairs = new int[z-1][2];
+		int[][] pairs = new int[t][2];
 		for (int i = 0; i < t; i++)
 			pairs[i] = new int[] {in.nextInt(), in.nextInt()};
 		in.close();
-		
+
 		for (int[] pair : pairs) {
 			int row = pair[0]-1;
 			int col = pair[1]-1;
@@ -24,7 +24,7 @@ public class GridSums {
 				sum += grid[r][col];
 			for (int c = 0; c < z; c++)
 				sum += grid[row][c];
-			
+
 			System.out.println(sum);
 		}
 	}
